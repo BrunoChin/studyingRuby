f8af76bfb79385cc5ced6645ce6cf2dae6e402c3
Bruno Henrique Silva de Oliveira <47747909+BrunoChin@users.noreply.github.com>
2022-06-01 19:11:47 -0300

    initial commit

---
+++ RubyTest/ArryaAndLoops.rb
@@ -0,0 +1,12 @@
+names = Array.new
+
+for a in 1..5 do
+    puts "Enter a Name: "
+    name = gets
+
+    names[names.length] = name
+
+end
+
+puts "-----------------------------"
+puts names
---
+++ RubyTest/math.rb
@@ -0,0 +1,9 @@
+puts "Enter a number: "
+num1 = gets.chomp().to_f
+puts "Enter another numver: "
+num2 = gets.chomp().to_f
+
+puts "Sum: " + (num1 + num2).to_s
+puts "Sub: " + (num1 - num2).to_s
+puts "Mult: " + (num1 * num2).to_s
+puts "Div: " + (num1 / num2).to_s
